6d4b78b2baae77217febe192f178a493a6ff1e76
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 16:54:59 -0300

    3a) Create an array with months and show (with console.log) months 5 and 11

---
+++ Exercises/03_arrays.js
@@ -0,0 +1,13 @@
+/************************************* 
+Third Exercise: Arrays
+**************************************/
+console.log('Third Exercise: Arrays');
+/************************************* 
+ ITEM A
+**************************************/
+console.log('Item 3A');
+var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre',
+ 'Noviembre', 'Diciembre'];
+console.log('Months Array: ' + months);
+console.log('Mes 5: ' + months[4]);
+console.log('Mes 5: ' + months[10]);
