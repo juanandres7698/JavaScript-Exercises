ff2a604caca2d188c6348bd2a8bd41a58cd4255b
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 16:42:57 -0300

    2c) Create string var and create another var with last 3 char

--- Exercises/02_strings.js
+++ Exercises/02_strings.js
@@ -17,3 +17,11 @@ var myWordTwo = 'AstonishingThings';
 var wordTwoSub = myWordTwo.substring(0,5);
 console.log('Variable myWordTwo: ' + myWordTwo);
 console.log('First 5 code units: ' + wordTwoSub);
+/************************************* 
+ ITEM C
+**************************************/
+console.log('Item 2C');
+var myWordThree = 'incomprehensibility';
+var wordThreeSub = myWordThree.substring (myWordThree.length - 3,myWordThree.length);
+console.log('Variable myWordThree: ' + myWordThree);
+console.log('Last 3 code units: ' + wordThreeSub);
