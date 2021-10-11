7851ccf38ac9563310da48832cd0a6003314361a
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 17:08:23 -0300

    4a) Create a random number between 0 and 1 (using Math Random) and c
    lassify it according to value (lower or equal/greater than 0.5

---
+++ Exercises/04_if_else.js
@@ -0,0 +1,17 @@
+/************************************* 
+Fourth Exercise: If else
+**************************************/
+console.log('Fourth Exercise: If else');
+/************************************* 
+ ITEM A
+**************************************/
+console.log('Item 4A');
+var number = Math.random();
+console.log(number);
+if (number >=0.5){
+    console.log('Greater than 0.5');
+    window.alert('Greater than 0.5');
+} else {
+    console.log('Lower than 0.5');
+    window.alert('Lower than 0.5');
+}
