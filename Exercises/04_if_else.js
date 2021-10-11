fab5b0096903845f0fbed1814562ce7cd6f14250
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 17:09:53 -0300

    4b) Create a numeric var Age with a number between 0 and 100. Classify it into different categories depending on the number (Bebe, Niño
    , Adolescente, Joven, ...)

--- Exercises/04_if_else.js
+++ Exercises/04_if_else.js
@@ -15,3 +15,44 @@ if (number >=0.5){
     console.log('Lower than 0.5');
     window.alert('Lower than 0.5');
 }
+/************************************* 
+ ITEM B
+**************************************/
+console.log('Item 4B');
+var age = Math.random()*101;
+//var age = 1;
+//var age = 9;
+//var age = 15;
+//var age = 26;
+//var age = 45;
+//var age = 67;
+//var age = 91;
+console.log(age);
+if (age<2) {
+    console.log('Bebe');
+    window.alert('Bebe');
+}
+else if (age>=2 && age<=12){
+    console.log('Niño');
+    window.alert('Niño');
+}
+else if (age>=13 && age<=19){
+    console.log('Adolescente');
+    window.alert('Adolescente');
+}
+else if (age>=20 && age<=30){
+    console.log('Joven');
+    window.alert('Joven');
+}
+else if (age>=31 && age<=60){
+    console.log('Adulto');
+    window.alert('Adulto');
+}
+else if (age>=61 && age<=75){
+    console.log('Adulto mayor');
+    window.alert('Adulto mayor');
+}
+else {
+    console.log('Anciano');
+    window.alert('Anciano');
+}
