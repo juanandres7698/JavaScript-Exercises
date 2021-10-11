8c54c14dae2c11d3ea603e592e88861b06a32e2b
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 16:41:36 -0300

    2b) Create string var and create another var with first 5 char

--- Exercises/02_strings.js
+++ Exercises/02_strings.js
@@ -9,3 +9,11 @@ console.log('Item 2A');
 var myWord = 'otolaryngologist';
 console.log('Word without uppercase: ' + myWord);
 console.log('Word with uppercase: ' + myWord.toUpperCase());
+/************************************* 
+ ITEM B
+**************************************/
+console.log('Item 2B');
+var myWordTwo = 'AstonishingThings';
+var wordTwoSub = myWordTwo.substring(0,5);
+console.log('Variable myWordTwo: ' + myWordTwo);
+console.log('First 5 code units: ' + wordTwoSub);
