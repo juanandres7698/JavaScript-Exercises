aa750fc44fb275c75facbfce6aed7799b2d7043f
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 17:26:31 -0300

    6b) Add a validation to the previous function, to control if the parameters received by func are numbers, showing NaN in case they aren´t

--- Exercises/06_functions.js
+++ Exercises/06_functions.js
@@ -16,3 +16,27 @@ result = addition (n1, n2);
 console.log('Number 1: ' + n1);
 console.log('Number 2: ' + n2);
 console.log('Result: ' + result);
+/************************************* 
+ ITEM B
+**************************************/
+console.log('Item 6B');
+function addition (number1,number2) {
+    if (isNaN(number1) || isNaN(number2)) {
+      return 'NaN';
+    } 
+    else {
+        return number1 + number2;
+    }
+}
+var n1, n2, result;
+n1 = 25;
+n2 = 'Peter';
+result = addition (n1, n2);
+console.log('Number 1: ' + n1);
+console.log('Number 2: ' + n2); 
+if (isNaN(n1) || isNaN(n2)) {
+   window.alert('At least one parameter in wrong')
+   console.log('Result: ' + result);
+} else {
+  console.log('Result: ' + result);
+}
