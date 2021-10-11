46911025bd0c07a24814c1b8b21cbe300b5f123a
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 17:12:04 -0300

    5a)Create an array with 5 elements and use for clause to loop through the code and show alert messages with elements

---
+++ Exercises/05_for.js
@@ -0,0 +1,15 @@
+/************************************* 
+Fifth Exercise: For
+**************************************/
+console.log('Fifth Exercise: For');
+/************************************* 
+ ITEM A
+**************************************/
+console.log('Item 5A');
+var numbers = new Array ('one','two','three','four','five');
+console.log('Array Numbers:');
+console.log(numbers);
+for (var i=0; i<=numbers.length-1; i++){
+    window.alert(numbers[i]);
+    console.log(numbers[i]);
+};
