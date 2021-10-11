cf109ca2ae4b133334959af0fc4fd7eb6e9c704c
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 17:24:41 -0300

    6a) Create a function that receive two numbers and add them. Then show the result by console

---
+++ Exercises/06_functions.js
@@ -0,0 +1,18 @@
+/************************************* 
+Sixth Exercise: Functions
+**************************************/
+console.log('Sixth Exercise: Functions');
+/************************************* 
+ ITEM A
+**************************************/
+console.log('Item 6A');
+function verifiedAddition (number1, number2){
+    return number1 + number2;
+};
+var n1, n2, result;
+n1 = 25;
+n2 = 97;
+result = addition (n1, n2);
+console.log('Number 1: ' + n1);
+console.log('Number 2: ' + n2);
+console.log('Result: ' + result);
