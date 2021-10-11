0650d064dc00e682c42a26527a85da947ef61e8c
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 17:29:26 -0300

    6d) Add to 6b function (Addition with NaN validation) a new validation to know if the result is integer or not, returning an alert message and the result number rounded in case it isn't

--- Exercises/06_functions.js
+++ Exercises/06_functions.js
@@ -55,3 +55,35 @@ var n1 = 45
 var validation = validateInteger(n1) 
 console.log (n1)
 console.log (validation)
+/************************************* 
+ ITEM D
+**************************************/
+console.log('Item 6D');
+function additionInteger (number1, number2) {
+    if ((isNaN(number1) || isNaN(number2)))  {
+        return 'NaN';
+      } 
+      else if (!(Number.isInteger(number1)) || !(Number.isInteger(number2))){
+          adding = number1 + number2
+          return Math.round (adding) 
+      }
+      else {
+          return number1 + number2
+    }
+}
+var n1, n2, result;
+n1 = 25;
+n2 = 27.8;
+result = additionInteger (n1, n2);
+console.log('Number 1: ' + n1);
+console.log('Number 2: ' + n2); 
+if (isNaN(n1) || isNaN(n2)) {
+   window.alert('At least one parameter in wrong')
+   console.log('Result: ' + result);
+} else if (!(Number.isInteger(n1)) || !(Number.isInteger(n2))) {
+    window.alert('Error: at least one number is not integer');
+    console.log('Result: ' + result);
+}
+else {
+    console.log('Result: ' + result);
+}
