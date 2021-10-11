f2a74fb172a434fe8acd3b3a59522e4f579a685c
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 16:36:15 -0300

    1a) Create two numeric var and add both numbers, add the result to a third var

---
+++ Exercises/01_variables_and_operators.js
@@ -0,0 +1,15 @@
+/************************************* 
+ First Exercise: Variables and operators
+**************************************/
+console.log('First Exercise: Variables and operators');
+/************************************* 
+ ITEM A
+**************************************/
+console.log('Item 1A');
+var firstNumber, secondNumber, addition;
+firstNumber = 23;
+secondNumber = 47.7;
+addition = firstNumber + secondNumber;
+console.log('First number: ' + firstNumber);
+console.log('Second Number: ' + secondNumber);
+console.log('Addition: ' + addition);
