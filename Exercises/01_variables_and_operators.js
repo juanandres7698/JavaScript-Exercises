9c167caba624375a8e02f4ec071f00a00361c9f4
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 16:38:21 -0300

    1c) Create two strings and add them in a third var. Calculate length of this var

--- Exercises/01_variables_and_operators.js
+++ Exercises/01_variables_and_operators.js
@@ -23,3 +23,16 @@ var completeName = firstName + lastName;
 console.log('First Name: ' + firstName);
 console.log('Last Name: ' + lastName);
 console.log('Complete name: ' + completeName);
+/************************************* 
+ ITEM C
+**************************************/
+console.log('Item 1C');
+var city, country;
+city = 'London';
+country = 'England';
+totalLength = city.length + country.length;
+console.log('Variable City: ' + city);
+console.log('Variable Country: ' + country);
+console.log('London is ' + city.length + ' code units long');
+console.log('England is ' + country.length + ' code units long');
+console.log('Total Length: ' + totalLength);
