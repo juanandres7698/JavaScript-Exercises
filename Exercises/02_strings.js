79acb6464712e0d6fac9b194ad49c596860a85bc
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-10-11 16:40:32 -0300

    2a) Create string var and transform it to uppercase

---
+++ Exercises/02_strings.js
@@ -0,0 +1,11 @@
+/************************************* 
+ Second Exercise: Strings
+**************************************/
+console.log('Second Exercise: Strings');
+/************************************* 
+ ITEM A
+**************************************/
+console.log('Item 2A');
+var myWord = 'otolaryngologist';
+console.log('Word without uppercase: ' + myWord);
+console.log('Word with uppercase: ' + myWord.toUpperCase());
